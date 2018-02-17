747c8c05c3f335706a7099e767a2022014dea75e
gustavoalegre <gustavoalegre1@hotmail.com>
2018-02-17 01:15:11 -0500

    HTTPS

--- bitecm_worker.js
+++ bitecm_worker.js
@@ -18,6 +18,8 @@ var options = {
 
 var queue = Jobs(db, worker, options);
 
+var https = require("https");
+
 module.exports = queue;
 
 function worker (id, payload, callback) {
@@ -31,23 +33,35 @@ function worker (id, payload, callback) {
 function sendRequest (payload, callback) {
   var err;
   var service = Services();
-  var servers = ['pe-ecomm01.viettelperu.com.pe', 'pe-ecomm02.viettelperu.com.pe'];
+  // El axios no reconoce dominios registrados en el /etc/hosts. Siempre busca en el DNS
+  var servers = ['10.121.6.249', '10.121.6.251']; //["pe-ecomm01.viettelperu.com.pe", "pe-ecomm02.viettelperu.com.pe"];
   var selectedServer = Math.floor(Math.random() * Math.floor(2));
   var alternateServer = (selectedServer == 0 ? 1 : 0);
   var selectedUrl = 'https://'+servers[selectedServer]+':8443/api/check_porting_status/'+payload.order_id;
   var alternateUrl = 'https://'+servers[alternateServer]+':8443/api/check_porting_status/'+payload.order_id;
+  
+  // Quita verificación de certificado SSL para poder conectarse a los ecommerce a través de la IP
+  const agent = new https.Agent({  
+    rejectUnauthorized: false
+  });
   //var url = 'https://10.121.8.25/api/check_porting_status/'+payload.order_id;
   //var url = 'http://bitel-store.dev/api/test/'+payload.order_id;
 
   // Intentar conexión con el servidor seleccionado
-  service.checkPortingStatus(selectedUrl, payload).then(function (success) {
+  console.log('Trying... %s', selectedUrl);
+  service.checkPortingStatus(selectedUrl, payload, {httpsAgent: agent}).then(function (success) {
+    console.log('Connecting... %s', servers[selectedServer]);
     if (!success){
-      console.log('Error in request to server %s. Trying to connect to server %s', selectedServer, alternateServer);
+      console.log('Error in request to server %s. Trying to connect to server %s', servers[selectedServer], servers[alternateServer]);
       // Intentar conexión con el servidor alternativo
-      service.checkPortingStatus(alternateUrl, payload).then(function (success) {
+      service.checkPortingStatus(alternateUrl, payload, {httpsAgent: agent}).then(function (success) {
         if (!success) err = Error('Error in request.');
+        else console.log('Sucess... %s', servers[alternateServer]);
       });
+    } else {
+      console.log('Sucess... %s', servers[selectedServer]);
     }
+    console.log('Returning callback...');
     callback(err);
   });
 }
